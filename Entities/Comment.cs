9ff43e86192fa1407c9d07d443d1dadab8987a1e
oguzhandilek <oguzhandilek25@gmail.com>
2024-02-12 14:28:11 +0300

    Commen update

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -5,6 +5,7 @@ namespace Entities;
 public class Comment:IEntity
 {
     public int Id { get; set; }
+    public int ChannelId { get; set; }
     public string Text { get; set; }
     public int UserId { get; set; }
     public DateTime CreatedAt { get; set; }
